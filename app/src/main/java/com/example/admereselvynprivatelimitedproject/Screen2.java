ed247ac907003fe60172adc7e31dd67984f4b277
Ashutosh Kumar <ashutoshhappy0007@gmail.com>
2021-01-13 21:57:25 +0530

    Done with screen2

--- app/src/main/java/com/example/admereselvynprivatelimitedproject/Screen2.java
+++ app/src/main/java/com/example/admereselvynprivatelimitedproject/Screen2.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Screen2 extends AppCompatActivity {
 
-    private Button enterNumber;
+    private LinearLayout enterNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
